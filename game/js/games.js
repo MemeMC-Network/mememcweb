80febd028b08e23e22e87c339b920e48ddfe251e
MemeMC <149332208+MemeMC-Network@users.noreply.github.com>
2024-11-11 14:41:52 -0500

    Update games.js

--- game/js/games.js
+++ game/js/games.js
@@ -8,8 +8,9 @@ document.addEventListener("DOMContentLoaded", function () {
         "game4": "https://class811.github.io/g177/skibidi-shooter/",
         "game5": "https://class811.github.io/g/ovo/",
         "game6": "https://class811.github.io/g3/stick-merge/",
-        "game7": "https://www.y8.com/games/slope",
+        "game7": "https://firespread01.github.io/slope/",
         "game8": "https://game316009.konggames.com/gamez/0031/6009/live/index.html/"
+        "game9": "https://script.google.com/a/macros/my.npsct.org/s/AKfycbw6e8fflbfydV7kom5id09nKaM6ix0hLlXHbs3XHOnxzrndUgPtHUHENrwKomI2Hpk3/exec"
     };
 
     const iframe = document.getElementById("gameFrame");
